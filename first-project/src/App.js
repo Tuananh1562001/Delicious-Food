a5d6bb11dd0d15a5e0817676cff55544cbee3fa7
Tuananh15062001 <tuananh15062001@gmail.com>
2022-04-09 18:09:32 +0700

    09/04/2021

---
+++ first-project/src/App.js
@@ -0,0 +1,12 @@
+import Pages from "./pages/Pages";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Pages />
+      {/* fetch('https://spoonacular.com/api&key="process"') */}
+    </div>
+  );
+}
+
+export default App;
